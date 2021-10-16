8064817ccf26e045bfad0c09d82237b94a7a0611
Emmanuel M. Achana <achanamosey@gmail.com>
2021-10-16 10:17:15 -0700

    Generated new proto requests.

--- proto/randomPackage/Random.ts
+++ proto/randomPackage/Random.ts
@@ -2,6 +2,8 @@
 
 import type * as grpc from '@grpc/grpc-js'
 import type { MethodDefinition } from '@grpc/proto-loader'
+import type { NumberRequest as _randomPackage_NumberRequest, NumberRequest__Output as _randomPackage_NumberRequest__Output } from '../randomPackage/NumberRequest';
+import type { NumberResponse as _randomPackage_NumberResponse, NumberResponse__Output as _randomPackage_NumberResponse__Output } from '../randomPackage/NumberResponse';
 import type { PingRequest as _randomPackage_PingRequest, PingRequest__Output as _randomPackage_PingRequest__Output } from '../randomPackage/PingRequest';
 import type { PongResponse as _randomPackage_PongResponse, PongResponse__Output as _randomPackage_PongResponse__Output } from '../randomPackage/PongResponse';
 
@@ -15,13 +17,25 @@ export interface RandomClient extends grpc.Client {
   pingPong(argument: _randomPackage_PingRequest, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _randomPackage_PongResponse__Output) => void): grpc.ClientUnaryCall;
   pingPong(argument: _randomPackage_PingRequest, callback: (error?: grpc.ServiceError, result?: _randomPackage_PongResponse__Output) => void): grpc.ClientUnaryCall;
   
+  RandomNumber(argument: _randomPackage_NumberRequest, metadata: grpc.Metadata, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  RandomNumber(argument: _randomPackage_NumberRequest, metadata: grpc.Metadata, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  RandomNumber(argument: _randomPackage_NumberRequest, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  RandomNumber(argument: _randomPackage_NumberRequest, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  randomNumber(argument: _randomPackage_NumberRequest, metadata: grpc.Metadata, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  randomNumber(argument: _randomPackage_NumberRequest, metadata: grpc.Metadata, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  randomNumber(argument: _randomPackage_NumberRequest, options: grpc.CallOptions, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  randomNumber(argument: _randomPackage_NumberRequest, callback: (error?: grpc.ServiceError, result?: _randomPackage_NumberResponse__Output) => void): grpc.ClientUnaryCall;
+  
 }
 
 export interface RandomHandlers extends grpc.UntypedServiceImplementation {
   PingPong: grpc.handleUnaryCall<_randomPackage_PingRequest__Output, _randomPackage_PongResponse>;
   
+  RandomNumber: grpc.handleUnaryCall<_randomPackage_NumberRequest__Output, _randomPackage_NumberResponse>;
+  
 }
 
 export interface RandomDefinition extends grpc.ServiceDefinition {
   PingPong: MethodDefinition<_randomPackage_PingRequest, _randomPackage_PongResponse, _randomPackage_PingRequest__Output, _randomPackage_PongResponse__Output>
+  RandomNumber: MethodDefinition<_randomPackage_NumberRequest, _randomPackage_NumberResponse, _randomPackage_NumberRequest__Output, _randomPackage_NumberResponse__Output>
 }
