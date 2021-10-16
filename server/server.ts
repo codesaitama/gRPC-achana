acd25b8b448786acfabae88fba1ee4603b91bee0
Emmanuel M. Achana <achanamosey@gmail.com>
2021-10-16 11:02:25 -0700

    Server calling the ToDo protos.

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,21 @@
 import path from 'path';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
-import {ProtoGrpcType} from '../proto/random';
-import {RandomHandlers} from '../proto/randomPackage/Random'
+import { ProtoGrpcType } from '../proto/random';
+import { RandomHandlers } from '../proto/randomPackage/Random'
 
 const PORT = 8082;
 const PROTO_FILE = '../proto/random.proto';
+const TODOS = [];
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
 const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as ProtoGrpcType;
 const randomPackage = grpcObject.randomPackage;
 
-function main(){
+function main() {
     const server = getServer();
-    server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) =>{
-        if(err){
+    server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+        if (err) {
             console.error(err);
             return;
         }
@@ -25,31 +26,39 @@ function main(){
     })
 }
 
-function getServer(){
-    const  server = new grpc.Server();
+function getServer() {
+    const server = new grpc.Server();
     server.addService(randomPackage.Random.service, {
         PingPong: (req, res) => {
-           // req.request;
+            // req.request;
             console.log(req.request);
-            res(null, {message: 'Pong'});
+            res(null, { message: 'Pong' });
         },
         RandomNumber: (call) => {
-            const {maxVal = 10} = call.request;
-           
+            const { maxVal = 10 } = call.request;
+
             let count = 0
             const id = setInterval(() => {
                 count = ++count;
 
-                call.write({num: Math.floor(Math.random() * maxVal)});
+                call.write({ num: Math.floor(Math.random() * maxVal) });
 
-                if(count >= 10){
+                if (count >= 10) {
                     clearInterval(id);
                     call.end();
                 }
 
-            }, 2000)
+            }, 500)
+        },
+        TodoList: (call, callback) => {
+            call.on('data', (chunk) => {
+                TODOS.push(chunk)
+                console.log(chunk)
+            });
 
-            
+            call.on('end', () => {
+                callback(null, {todos: TODOS})
+            });
         }
     } as RandomHandlers);
 
