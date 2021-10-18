3422fd38bbd4b16fb427f356a1740e030a4ff4c5
Emmanuel M. Achana <achanamosey@gmail.com>
2021-10-17 23:03:53 -0700

    Sync the project.

--- server/server.ts
+++ server/server.ts
@@ -4,12 +4,15 @@ import * as protoLoader from '@grpc/proto-loader';
 import { ProtoGrpcType } from '../proto/random';
 import { RandomHandlers } from '../proto/randomPackage/Random'
 import { TodoResponse } from '../proto/randomPackage/TodoResponse';
+import { ChatRequest } from '../proto/randomPackage/ChatRequest';
+import { ChatResponse } from '../proto/randomPackage/ChatResponse';
 
 const PORT = 8082;
 const PROTO_FILE = '../proto/random.proto';
 
 const TODOS :Array<object> = [];
 //const _TODOS :TodoResponse = {todos: []};
+const callObjByUsername = new Map<string, grpc.ServerDuplexStream<ChatRequest, ChatResponse>>()
 
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
 const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as ProtoGrpcType;
@@ -62,7 +65,48 @@ function getServer() {
             call.on('end', () => {
                 callback(null, {todos: TODOS})
             });
-        }
+        },
+        Chat: (call) => {
+            call.on("data", (req) => {
+              const username = call.metadata.get('username')[0] as string
+              const msg = req.message
+              console.log(username, req.message)
+      
+      
+              for(let [user, usersCall] of callObjByUsername) {
+                if(username !== user) {
+                  usersCall.write({
+                    username: username,
+                    message: msg
+                  })
+                }
+              }
+      
+              if (callObjByUsername.get(username) === undefined) {
+                callObjByUsername.set(username, call)
+              }
+            })
+      
+            call.on("end", () => {
+              const username = call.metadata.get('username')[0] as string
+              callObjByUsername.delete(username)
+              for(let [user, usersCall] of callObjByUsername) {
+                  usersCall.write({
+                    username: username,
+                    message: "Has Left the Chat!"
+                  })
+              }
+              console.log(`${username} is ending their chat session`)
+      
+              call.write({
+                username: "Server",
+                message: `See you later ${username}`
+              })
+      
+              call.end()
+            })
+      
+          }
     } as RandomHandlers);
 
     return server;
